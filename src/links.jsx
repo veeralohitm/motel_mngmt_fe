477515585745ba81f01ead89c3094a031901dc1e
Veera Lohit <veeralohitm@Veeras-MacBook-Air.local>
2025-03-02 23:42:15 -0500

    new url

--- src/links.jsx
+++ src/links.jsx
@@ -1,3 +1,3 @@
 
 //export const BASE_URL = "http://localhost:5000/api/";
-export const BASE_URL = "https://hotel-app-bk.onrender.com/api"
+export const BASE_URL = "https://hotel-app-bk.onrender.com/api/"
